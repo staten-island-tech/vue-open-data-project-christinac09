6d7c910caf3f95089f8dfd1cbf0f68f4b8a6d08b
christina <158211764+christinac09@users.noreply.github.com>
2025-03-03 13:57:12 -0500

    Update HomeView.vue

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,9 +1,18 @@
+<template>
+  <div></div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from 'vue'
+const arrestData = ref('')
+async function getPokemon() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.json')
+  let data = await res.json()
+  arrestData.value = data.results
+}
+onMounted(() => {
+  getPokemon()
+})
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped></style>
