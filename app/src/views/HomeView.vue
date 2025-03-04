ddc6f37e53e8dc18e557938fb683ca7fc0b8f612
christina <158211764+christinac09@users.noreply.github.com>
2025-03-04 14:00:24 -0500

    fix tailwind & daisyui

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,5 +1,10 @@
 <template>
-  <div></div>
+  <div>
+    <!-- <div class="card" v-for="a in arrestData" :key="a.arrest_key">
+      <h2>{{ a.arrest_boro }}</h2>
+    </div> -->
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  </div>
 </template>
 
 <script setup>
@@ -8,7 +13,7 @@ const arrestData = ref('')
 async function getArrests() {
   let res = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.json')
   let data = await res.json()
-  arrestData.value = data.results
+  arrestData.value = data
 }
 onMounted(() => {
   getArrests()
