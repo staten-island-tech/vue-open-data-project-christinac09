a2b37100cfd996a39f0fba7a2ea318c9c463f1a5
christina <158211764+christinac09@users.noreply.github.com>
2025-03-05 13:48:51 -0500

    Revert "fix tailwind & daisyui"
    This reverts commit ddc6f37e53e8dc18e557938fb683ca7fc0b8f612.

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <!-- <div class="card" v-for="a in arrestData" :key="a.arrest_key">
-      <h2>{{ a.arrest_boro }}</h2>
-    </div> -->
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  </div>
+  <div></div>
 </template>
 
 <script setup>
@@ -13,7 +8,7 @@ const arrestData = ref('')
 async function getArrests() {
   let res = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.json')
   let data = await res.json()
-  arrestData.value = data
+  arrestData.value = data.results
 }
 onMounted(() => {
   getArrests()
