1d9a1534cbff8e69ef4eeee106bbeec8f7e5ef6e
christina <158211764+christinac09@users.noreply.github.com>
2025-03-06 13:05:20 -0500

    fix tailwind

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-[![Open in Codespaces](https://classroom.github.com/assets/launch-codespace-2972f46106e565e64193e422d61a12cf1da4916b45550586e14ef0a7c637dd04.svg)](https://classroom.github.com/open-in-codespaces?assignment_repo_id=18423701)
+# Vue 3 + Vite
+
+This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+
+Learn more about IDE Support for Vue in the [Vue Docs Scaling up Guide](https://vuejs.org/guide/scaling-up/tooling.html#ide-support).
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,18 +1,13 @@
-<template>
-  <div></div>
-</template>
-
 <script setup>
-import { ref, onMounted } from 'vue'
-const arrestData = ref('')
-async function getArrests() {
-  let res = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.json')
-  let data = await res.json()
-  arrestData.value = data.results
-}
-onMounted(() => {
-  getArrests()
-})
+import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
-<style scoped></style>
+<template>
+  <main>
+    <div class="container mx-auto p-4">
+      <h1 class="text-3xl font-bold text-center mb-4">Hello Vite!</h1>
+      <button class="btn btn-primary bg-black">Click Me</button>
+    </div>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  </main>
+</template>
