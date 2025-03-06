450aa6d2dbd3369e3df178dc57b1681d2c6de2ec
christina <158211764+christinac09@users.noreply.github.com>
2025-03-06 12:37:25 -0500

    Revert "what am i doing"
    This reverts commit ad7d327e247f08bef04cc7d3f32bb8a13510d1b3.

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,28 +1,14 @@
 <template>
-  <div>
-    <div class="container mx-auto p-4">
-      <h1 class="text-3xl font-bold text-center mb-4">Hello Vite!</h1>
-      <button class="btn btn-primary">Click Me</button>
-    </div>
-  </div>
+  <div></div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 const arrestData = ref('')
 async function getArrests() {
-  try {
-    const response = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.json')
-    if (response.status != 200) {
-      throw new Error(response)
-    } else {
-      const data = await response.json()
-      arrestData.value = data
-      return data
-    }
-  } catch (error) {
-    alert('could not find that character')
-  }
+  let res = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.json')
+  let data = await res.json()
+  arrestData.value = data.results
 }
 onMounted(() => {
   getArrests()
